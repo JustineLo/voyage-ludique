cbbbf1db22a7f05d81abc76bbaeb12d17969fecd
Justine Lo <justine@ecotree.fr>
2023-07-05 21:27:48 +0200

    add post, delete, update, get api routes

--- frontend/src/services/gameService.tsx
+++ frontend/src/services/gameService.tsx
@@ -8,4 +8,24 @@ const getAll = async (): Promise<Game[]> => {
   return response.data;
 };
 
-export default { getAll };
+const getOne = async (id: number) => {
+    const response = await axios.get(`${baseUrl}/${id}`);
+    return response.data;
+};
+  
+const create = async (newGame: Game) => {
+const response = await axios.post(baseUrl, newGame);
+return response.data;
+};
+
+const update = async (id: number, updatedGame: Game) => {
+const response = await axios.put(`${baseUrl}/${id}`, updatedGame);
+return response.data;
+};
+
+const remove = async (id: number) => {
+const response = await axios.delete(`${baseUrl}/${id}`);
+return response.data;
+};
+  
+  export default { getAll, getOne, create, update, remove };
