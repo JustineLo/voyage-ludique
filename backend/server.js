6c65e4940ca22a37dccf94378c16bd39956cf3d5
Justine Lo <justine@ecotree.fr>
2023-07-05 21:54:08 +0200

    add cors and json to back server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
 const gameRoutes = require('./routes/gameRoutes');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to Game Tracker API!');
