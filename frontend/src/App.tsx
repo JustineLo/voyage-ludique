a79606da8a0d2ea00b5193c620f7205bf5b9e246
Justine Lo <justine@ecotree.fr>
2023-07-05 07:35:36 +0200

    add context provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import { useReducer } from "react";
+import { gameReducer } from "./reducers/gameReducer";
+import { GameContext } from "./contexts/GameContext";
+
+const initialState = {
+  games: [],
+};
+
 function App() {
 
-  return (
-    <>
+  const [state, dispatch] = useReducer(gameReducer, initialState);
 
-    </>
-  )
+  return (
+      <GameContext.Provider value={{ state, dispatch }}>
+        <></>
+      </GameContext.Provider>
+  );
 }
 
 export default App
