d0a45e9b36c47c11103935ad67e18e798aa819ac
Justine Lo <justine@ecotree.fr>
2023-07-08 00:05:41 +0200

    move service

---
+++ frontend/src/services/moveService.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Move } from '../types';
+
+const baseUrl = 'http://localhost:5001/api/moves'; 
+
+export const getMovesByGameIdAPI = async (gameId: number) => {
+  const response = await axios.get(`${baseUrl}/games/${gameId}/moves`);
+  return response.data;
+};
+
+export const getMoveAPI = async (moveId: number) => {
+  const response = await axios.get(`${baseUrl}/moves/${moveId}`);
+  return response.data;
+};
+
+export const createMoveAPI = async (move: Move) => {
+  const response = await axios.post(`${baseUrl}`, move);
+  return response.data;
+};
+
+export const updateMoveAPI = async (moveId: number, move: Partial<Move>) => {
+  const response = await axios.put(`${baseUrl}/moves/${moveId}`, move);
+  return response.data;
+};
+
+export const deleteMoveAPI = async (moveId: number) => {
+  const response = await axios.delete(`${baseUrl}/moves/${moveId}`);
+  return response.data;
+};
