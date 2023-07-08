cca9f5f494aa70c05baac826bed414b07bb53989
Justine Lo <justine@ecotree.fr>
2023-07-09 01:09:03 +0200

    console

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 const DB_URL = process.env.DB_URL;
 const DB_KEY = process.env.DB_KEY;
-
+console.log(DB_URL, DB_KEY);
 const database = createClient(DB_URL, DB_KEY);
 
 module.exports = database;
