b5daeb0e9ff96a3437b180b47e9fdc3722ea88b8
Justine Lo <justine@ecotree.fr>
2023-08-27 20:52:31 +0200

    add buttons to Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ import { getAllGamesAPI } from "./services/gameService";
 import { getAllMovesAPI } from "./services/moveService";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./layout/theme";
-
+import NewMove from "./pages/NewMove";
 
 function App() {
   const [state, dispatch] = useReducer(gameReducer, defaultState);
@@ -19,8 +19,8 @@ function App() {
       try {
         const games = await getAllGamesAPI();
         const allMoves = await getAllMovesAPI();
-        dispatch({ type: 'SET_GAMES', payload: games });
-        dispatch({ type: 'SET_ALL_MOVES', payload: allMoves });
+        dispatch({ type: "SET_GAMES", payload: games });
+        dispatch({ type: "SET_ALL_MOVES", payload: allMoves });
       } catch (error) {
         console.error(error);
       }
@@ -29,20 +29,21 @@ function App() {
   }, []);
 
   return (
-      <GameContext.Provider value={{ state, dispatch }}>
-        <ThemeProvider theme={theme}>
+    <GameContext.Provider value={{ state, dispatch }}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
           <Layout>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path='ajouter-jeu' element={<NewGame />} />
-                <Route path='jeu/:gameId' element={<GameDetail />} />
-              </Routes>
-            </BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="ajouter-jeu" element={<NewGame />} />
+              <Route path="ajouter-mouvement" element={<NewMove />} />
+              <Route path="jeu/:gameId" element={<GameDetail />} />
+            </Routes>
           </Layout>
-        </ThemeProvider>
-      </GameContext.Provider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </GameContext.Provider>
   );
 }
 
-export default App
+export default App;
--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const AppbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--primary);
+  color: white;
+  padding: 1rem 2.6rem;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  button {
+    color: white;
+    background-color: var(--secondary);
+    padding: 0.5rem 1rem;
+  }
+`;
 
 function Navbar() {
-    return (
-        <AppBar position="static">
-            <Box p={2}>
-            <Typography component="div" sx={{ color: "white" }}>
-                Voyage ludique
-            </Typography>
-            </Box>
-        </AppBar>
-    )
+  const navigate = useNavigate();
+  return (
+    <AppbarContainer>
+      <h2>
+        <Link to="/">Voyage Ludique</Link>
+      </h2>
+      <Buttons>
+        <Button onClick={() => navigate("/ajouter-jeu")}>Ajouter un jeu</Button>
+        <Button onClick={() => navigate("/ajouter-mouvement")}>
+          Ajouter un mouvement
+        </Button>
+      </Buttons>
+    </AppbarContainer>
+  );
 }
 
 export default Navbar;
